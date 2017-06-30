23a78765b093263516737f842839cdb8473d4c36
dorayo <yomingyo@gmail.com>
2017-06-30 08:28:38 +0800

    init Project

---
+++ MainMulde/app/src/main/java/mainmulde/jojo/com/mainmulde/MainActivity.java
@@ -0,0 +1,13 @@
+package mainmulde.jojo.com.mainmulde;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
